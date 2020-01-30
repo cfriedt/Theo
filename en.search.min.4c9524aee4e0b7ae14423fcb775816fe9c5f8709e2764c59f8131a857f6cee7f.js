8d476bd50da224bab41768f70b687e1030650b89
Greg Davill <greg.davill@gmail.com>
2020-01-30 21:00:43 +1030

    Publishing to gh-pages (publish.sh)

--- en.search.min.4c9524aee4e0b7ae14423fcb775816fe9c5f8709e2764c59f8131a857f6cee7f.js
+++ en.search.min.4c9524aee4e0b7ae14423fcb775816fe9c5f8709e2764c59f8131a857f6cee7f.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/OrangeCrab/flexsearch.min.js');loadScript('/OrangeCrab/en.search-data.min.c55abab6e34741d471b3c5d0fc4e1a26c119516f7ad53f4f944948aa4de46426.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/OrangeCrab/flexsearch.min.js');loadScript('/OrangeCrab/en.search-data.min.0ef345f159a7a1f56a1a291a8c1912b4a51448193faadf7ed0902719b5d9e741.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
